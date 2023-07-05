3f345fe98e4f301828b79f9572f7ae25320b314a
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-05 16:14:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('my-cache').then(cache => {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
       return cache.addAll([
         '/',
         '/index.html',
-        '/icon.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
