e97992699f2ba1fba7eaed1581326618beb7b8c6
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-06 16:03:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,283 @@
+    const chatHistory = document.getElementById('chat-history');
+    const apiKeyInput = document.getElementById('api-key-input');
+    const apiEndpointInput = document.getElementById('api-endpoint-input');
+    const messageInput = document.getElementById('message-input');
+    const modelMenu = document.getElementById('model-menu');
+    const aiThinkingMsg = document.getElementById('ai-thinking');
+    const systemRoleInput = document.getElementById('system-role-input');
+    let messages = [
+      {
+        "role": "system",
+        "content": localStorage.getItem('systemRole') || "You are a helpful assistant."
+      }
+    ];
+
+    // Retrieve API key, API endpoint, and selected model from local storage if available
+    let apiKey = localStorage.getItem('apiKey');
+    let apiEndpoint = localStorage.getItem('apiEndpoint');
+    let selectedModel = localStorage.getItem('selectedModel');
+
+    if (!apiKey) {
+      apiKeyInput.value = '';
+    } else {
+      apiKeyInput.value = apiKey;
+    }
+
+    if (!apiEndpoint) {
+      apiEndpointInput.value = '';
+    } else {
+      apiEndpointInput.value = apiEndpoint;
+    }
+
+    if (!selectedModel) {
+      selectedModel = 'gpt-3.5-turbo';
+      const modelOption = document.querySelector(`ul li[data-model="${selectedModel}"]`);
+      if (modelOption) {
+        modelOption.classList.add('selected');
+      }
+    } else {
+      const modelOption = document.querySelector(`ul li[data-model="${selectedModel}"]`);
+      if (modelOption) {
+        modelOption.classList.add('selected');
+      }
+    }
+
+    messageInput.addEventListener('input', () => {
+      messageInput.style.height = 'auto';
+      messageInput.style.height = `${messageInput.scrollHeight}px`;
+    });
+
+    messageInput.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        messageInput.value += '\n';
+        messageInput.style.height = `${messageInput.scrollHeight}px`;
+      }
+    });
+
+    document.getElementById('send-button').addEventListener('click', sendMessage);
+
+    function toggleModelMenu() {
+      modelMenu.style.display = (modelMenu.style.display === 'none') ? 'block' : 'none';
+    }
+
+    function selectModel(model) {
+      const modelOptions = document.querySelectorAll('ul li');
+      modelOptions.forEach(option => option.classList.remove('selected'));
+
+      const selectedModelOption = document.querySelector(`ul li[data-model="${model}"]`);
+      if (selectedModelOption) {
+        selectedModelOption.classList.add('selected');
+      }
+
+      selectedModel = model;
+      localStorage.setItem('selectedModel', selectedModel);
+
+      toggleModelMenu();
+      updateModelHeading();
+    }
+
+    function updateModelHeading() {
+      const modelHeading = document.querySelector('h1');
+      modelHeading.textContent = `Chat with ${selectedModel}`;
+    }
+
+    async function getBotResponse(apiKey, apiEndpoint, message) {
+      const ENDPOINT = apiEndpoint || "https://chimeragpt.adventblocks.cc/v1/chat/completions";
+      const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      };
+
+      let maxTokens;
+      switch (selectedModel) {
+        case 'gpt-3.5-turbo':
+          maxTokens = 4096;
+          break;
+        case 'gpt-4-poe':
+          maxTokens = 2100;
+          break;
+        case 'gpt-3.5-turbo-16k':
+          maxTokens = 16384;
+          break;
+        case 'gpt-3.5-turbo-0613':
+          maxTokens = 4096;
+          break;
+        case 'gpt-4-0613':
+        case 'gpt-4':
+          maxTokens = 8192;
+          break;
+        case 'claude+':
+        case 'claude-instant':
+        case 'claude-instant-100k':
+          maxTokens = 10240;
+          break;
+        default:
+          maxTokens = 4096;
+      }
+
+      let tokenCount = getTokenCount(messages[0].content);
+      for (let i = 1; i < messages.length; i++) {
+        const messageTokenCount = getTokenCount(messages[i].content);
+        if (tokenCount + messageTokenCount > maxTokens) {
+          messages.splice(1, i - 1);
+          break;
+        }
+        tokenCount += messageTokenCount;
+      }
+
+      messages.push({
+        "role": "user",
+        "content": message
+      });
+
+      aiThinkingMsg.style.display = 'block';
+
+      const data = {
+        "model": selectedModel,
+        "messages": messages
+      };
+
+      const response = await fetch(ENDPOINT, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+      });
+
+      aiThinkingMsg.style.display = 'none';
+
+      return response.json();
+    }
+
+    function getTokenCount(text) {
+      const words = text.trim().split(/\s+/);
+      return words.length;
+    }
+
+    async function createAndAppendMessage(content, owner) {
+      const message = document.createElement('div');
+      message.classList.add('message', owner);
+
+      let displayedText = content;
+
+      const parsedContent = parseResponse(displayedText);
+      message.innerHTML = parsedContent;
+
+      chatHistory.appendChild(message);
+      chatHistory.scrollTop = chatHistory.scrollHeight;
+
+      // Render MathJax equations
+      MathJax.Hub.Queue(['Typeset', MathJax.Hub, message]);
+    }
+
+    function parseResponse(response) {
+      let parsedResponse = response;
+
+      parsedResponse = parsedResponse.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+      parsedResponse = parsedResponse.replace(/\$\$(.*?)\$\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
+      parsedResponse = parsedResponse.replace(/\$(.*?)\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
+      parsedResponse = parseTables(parsedResponse);
+
+      return parsedResponse;
+    }
+
+    function parseTables(response) {
+      const tableRegex = /\n((?:\s*\|.*\|\n)+)\n/g;
+      return response.replace(tableRegex, createTable);
+    }
+
+    function createTable(match, table) {
+      const rows = table.trim().split('\n');
+      const tableElement = document.createElement('table');
+
+      const tableHeader = document.createElement('tr');
+      const tableHeaderCells = rows[0].split('|').slice(1, -1);
+      tableHeaderCells.forEach((cell) => {
+        const th = document.createElement('th');
+        th.classList.add('table-header');
+        th.textContent = cell.trim();
+        tableHeader.appendChild(th);
+      });
+      tableElement.appendChild(tableHeader);
+
+      for (let i = 2; i < rows.length; i++) {
+        const row = document.createElement('tr');
+        const tableCells = rows[i].split('|').slice(1, -1);
+        tableCells.forEach((cell) => {
+          const td = document.createElement('td');
+          td.classList.add('table-data');
+          td.innerHTML = parseResponse(cell.trim());
+          row.appendChild(td);
+        });
+        tableElement.appendChild(row);
+      }
+
+      return tableElement.outerHTML;
+    }
+
+    async function sendMessage() {
+      apiKey = apiKeyInput.value.trim();
+      apiEndpoint = apiEndpointInput.value.trim();
+      const message = messageInput.value.trim();
+
+      if (!apiKey || !message) {
+        alert('Please enter your API key and a message.');
+        return;
+      }
+
+      localStorage.setItem('apiKey', apiKey);
+      localStorage.setItem('apiEndpoint', apiEndpoint);
+
+      createAndAppendMessage(message, 'user');
+      messageInput.value = '';
+      messageInput.style.height = 'auto';
+
+      const jsonResponse = await getBotResponse(apiKey, apiEndpoint, message);
+
+      const botResponse = jsonResponse.choices[0].message.content;
+      messages.push({
+        "role": "assistant",
+        "content": botResponse
+      });
+
+      createAndAppendMessage(botResponse, 'bot');
+    }
+
+    function copyToClipboard(text) {
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+    }
+
+    document.getElementById('copy-button').addEventListener('click', () => {
+      const latestResponse = chatHistory.lastElementChild.innerHTML;
+      if (latestResponse) {
+        copyToClipboard(latestResponse);
+        alert('Text copied to clipboard');
+      } else {
+        alert('No text to copy');
+      }
+    });
+
+    systemRoleInput.value = localStorage.getItem('systemRole') || "You are a helpful assistant.";
+    systemRoleInput.addEventListener('input', () => {
+      localStorage.setItem('systemRole', systemRoleInput.value);
+      messages[0].content = systemRoleInput.value;
+    });
+
+    window.addEventListener('load', () => {
+      updateModelHeading();
+    });
+
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(function(error) {
+          console.log('Service Worker registration failed:', error);
+        });
+    }
