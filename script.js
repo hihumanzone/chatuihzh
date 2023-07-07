d8e0dae673991b628d3ae063bbfd122c1ceb3432
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-07 14:00:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -271,13 +271,3 @@
     window.addEventListener('load', () => {
       updateModelHeading();
     });
-
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(function(registration) {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(function(error) {
-          console.log('Service Worker registration failed:', error);
-        });
-        }
