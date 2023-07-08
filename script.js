72b87f98268b66c5ca6e4956f1262b6664f9dd95
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-08 15:36:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,284 +1,269 @@
-class ChatApp {
-  constructor() {
-    this.chatHistory = document.getElementById('chat-history');
-    this.apiKeyInput = document.getElementById('api-key-input');
-    this.apiEndpointInput = document.getElementById('api-endpoint-input');
-    this.messageInput = document.getElementById('message-input');
-    this.modelMenu = document.getElementById('model-menu');
-    this.aiThinkingMsg = document.getElementById('ai-thinking');
-    this.systemRoleInput = document.getElementById('system-role-input');
-    this.selectedModel = 'gpt-3.5-turbo';
-    this.messages = [
-      {
-        role: 'system',
-        content: localStorage.getItem('systemRole') || 'You are a helpful assistant.',
-      },
-    ];
-  }
+// Selecting elements
+const chatHistory = document.getElementById('chat-history');
+const apiKeyInput = document.getElementById('api-key-input');
+const apiEndpointInput = document.getElementById('api-endpoint-input');
+const messageInput = document.getElementById('message-input');
+const modelMenu = document.getElementById('model-menu');
+const aiThinkingMsg = document.getElementById('ai-thinking');
+const systemRoleInput = document.getElementById('system-role-input');
 
-  init() {
-    this.loadApiDetails();
-    this.loadSelectedModel();
-    this.updateModelHeading();
-    this.addEventListeners();
-  }
+// Initializing variables
+let messages = [];
+let apiKey = "";
+let apiEndpoint = "";
+let selectedModel = "";
 
-  loadApiDetails() {
-    const apiKey = localStorage.getItem('apiKey');
-    const apiEndpoint = localStorage.getItem('apiEndpoint');
+// Retrieve API key, API endpoint, and selected model from local storage if available
+apiKeyInput.value = localStorage.getItem('apiKey') || "";
+apiEndpointInput.value = localStorage.getItem('apiEndpoint') || "";
+selectedModel = localStorage.getItem('selectedModel') || 'gpt-3.5-turbo';
 
-    this.apiKeyInput.value = apiKey || '';
-    this.apiEndpointInput.value = apiEndpoint || '';
-  }
+// Update model menu and heading
+updateModelMenu();
+updateModelHeading();
 
-  loadSelectedModel() {
-    const selectedModel = localStorage.getItem('selectedModel');
+// Set system role message
+systemRoleInput.value = localStorage.getItem('systemRole') || "You are a helpful assistant.";
 
-    if (selectedModel) {
-      this.selectedModel = selectedModel;
-      const modelOption = document.querySelector(`ul li[data-model="${selectedModel}"]`);
+// Add event listeners
+messageInput.addEventListener('input', adjustMessageInputHeight);
+messageInput.addEventListener('keydown', handleKeyDownEvent);
+document.getElementById('send-button').addEventListener('click', sendMessage);
+document.getElementById('copy-button').addEventListener('click', copyLatestResponse);
+systemRoleInput.addEventListener('input', updateSystemRoleMessage);
+window.addEventListener('load', updateModelHeading);
 
-      if (modelOption) {
-        modelOption.classList.add('selected');
-      }
-    }
-  }
+// Adjust message input height dynamically
+function adjustMessageInputHeight() {
+  messageInput.style.height = 'auto';
+  messageInput.style.height = `${messageInput.scrollHeight}px`;
+}
 
-  addEventListeners() {
-    this.messageInput.addEventListener('input', () => {
-      this.messageInput.style.height = 'auto';
-      this.messageInput.style.height = `${this.messageInput.scrollHeight}px`;
-    });
-
-    this.messageInput.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter' && !event.shiftKey) {
-        event.preventDefault();
-        this.sendMessage();
-      }
-    });
-
-    document.getElementById('send-button').addEventListener('click', () => {
-      this.sendMessage();
-    });
-
-    document.getElementById('copy-button').addEventListener('click', () => {
-      const latestResponse = this.chatHistory.lastElementChild.innerHTML;
-      if (latestResponse) {
-        this.copyToClipboard(latestResponse);
-        alert('Text copied to clipboard');
-      } else {
-        alert('No text to copy');
-      }
-    });
-
-    this.systemRoleInput.addEventListener('input', () => {
-      localStorage.setItem('systemRole', this.systemRoleInput.value);
-      this.messages[0].content = this.systemRoleInput.value;
-    });
-
-    window.addEventListener('load', () => {
-      this.updateModelHeading();
-    });
-  }
-
-  async getBotResponse(message) {
-    const ENDPOINT = this.apiEndpointInput.value || 'https://chimeragpt.adventblocks.cc/v1/chat/completions';
-    const apiKey = this.apiKeyInput.value;
-    const headers = {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${apiKey}`,
-    };
-
-    const maxTokens = this.getMaxTokens();
-
-    let tokenCount = this.getTokenCount(this.messages[0].content);
-    for (let i = 1; i < this.messages.length; i++) {
-      const messageTokenCount = this.getTokenCount(this.messages[i].content);
-      if (tokenCount + messageTokenCount > maxTokens) {
-        this.messages.splice(1, i - 1);
-        break;
-      }
-      tokenCount += messageTokenCount;
-    }
-
-    this.messages.push({
-      role: 'user',
-      content: message,
-    });
-
-    this.aiThinkingMsg.style.display = 'block';
-
-    const data = {
-      model: this.selectedModel,
-      messages: this.messages,
-    };
-
-    try {
-      const response = await fetch(ENDPOINT, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(data),
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch response.');
-      }
-
-      const jsonResponse = await response.json();
-      const botResponse = jsonResponse.choices[0].message.content;
-
-      this.messages.push({
-        role: 'assistant',
-        content: botResponse,
-      });
-
-      this.createAndAppendMessage(botResponse, 'bot');
-    } catch (error) {
-      console.error('Error:', error);
-      alert('An error occurred while fetching the bot response.');
-    } finally {
-      this.aiThinkingMsg.style.display = 'none';
-    }
-  }
-
-  getTokenCount(text) {
-    const words = text.trim().split(/\s+/);
-    return words.length;
-  }
-
-  getMaxTokens() {
-    switch (this.selectedModel) {
-      case 'gpt-3.5-turbo':
-        return 4096;
-      case 'gpt-4-poe':
-        return 2100;
-      case 'gpt-3.5-turbo-16k':
-        return 16384;
-      case 'gpt-3.5-turbo-0613':
-        return 4096;
-      case 'gpt-4-0613':
-      case 'gpt-4':
-        return 8192;
-      case 'claude+':
-      case 'claude-instant':
-      case 'claude-instant-100k':
-        return 10240;
-      default:
-        return 4096;
-    }
-  }
-
-  async sendMessage() {
-    const apiKey = this.apiKeyInput.value.trim();
-    const message = this.messageInput.value.trim();
-
-    if (!apiKey || !message) {
-      alert('Please enter your API key and a message.');
-      return;
-    }
-
-    localStorage.setItem('apiKey', apiKey);
-    localStorage.setItem('apiEndpoint', this.apiEndpointInput.value);
-
-    this.createAndAppendMessage(message, 'user');
-    this.messageInput.value = '';
-    this.messageInput.style.height = 'auto';
-
-    await this.getBotResponse(message);
-  }
-
-  copyToClipboard(text) {
-    const textarea = document.createElement('textarea');
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
-  }
-
-  createAndAppendMessage(content, owner) {
-    const message = document.createElement('div');
-    message.classList.add('message', owner);
-
-    let displayedText = content;
-
-    const parsedContent = this.parseResponse(displayedText);
-    message.innerHTML = parsedContent;
-
-    this.chatHistory.appendChild(message);
-    this.chatHistory.scrollTop = this.chatHistory.scrollHeight;
-  }
-
-  parseResponse(response) {
-    let parsedResponse = response;
-
-    parsedResponse = parsedResponse.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-    parsedResponse = parsedResponse.replace(/\$\$(.*?)\$\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
-    parsedResponse = parsedResponse.replace(/\$(.*?)\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
-    parsedResponse = this.parseTables(parsedResponse);
-
-    return parsedResponse;
-  }
-
-  parseTables(response) {
-    const tableRegex = /\n((?:\s*\|.*\|\n)+)\n/g;
-    return response.replace(tableRegex, this.createTable);
-  }
-
-  createTable(match, table) {
-    const rows = table.trim().split('\n');
-    const tableElement = document.createElement('table');
-
-    const tableHeader = document.createElement('tr');
-    const tableHeaderCells = rows[0].split('|').slice(1, -1);
-    tableHeaderCells.forEach((cell) => {
-      const th = document.createElement('th');
-      th.classList.add('table-header');
-      th.textContent = cell.trim();
-      tableHeader.appendChild(th);
-    });
-    tableElement.appendChild(tableHeader);
-
-    for (let i = 2; i < rows.length; i++) {
-      const row = document.createElement('tr');
-      const tableCells = rows[i].split('|').slice(1, -1);
-      tableCells.forEach((cell) => {
-        const td = document.createElement('td');
-        td.classList.add('table-data');
-        td.innerHTML = this.parseResponse(cell.trim());
-        row.appendChild(td);
-      });
-      tableElement.appendChild(row);
-    }
-
-    return tableElement.outerHTML;
-  }
-
-  toggleModelMenu() {
-    this.modelMenu.style.display = this.modelMenu.style.display === 'none' ? 'block' : 'none';
-  }
-
-  selectModel(model) {
-    const modelOptions = document.querySelectorAll('ul li');
-    modelOptions.forEach((option) => option.classList.remove('selected'));
-
-    const selectedModelOption = document.querySelector(`ul li[data-model="${model}"]`);
-    if (selectedModelOption) {
-      selectedModelOption.classList.add('selected');
-    }
-
-    this.selectedModel = model;
-    localStorage.setItem('selectedModel', this.selectedModel);
-
-    this.toggleModelMenu();
-    this.updateModelHeading();
-  }
-
-  updateModelHeading() {
-    const modelHeading = document.querySelector('h1');
-    modelHeading.textContent = `Chat with ${this.selectedModel}`;
+// Handle Enter key event in message input
+function handleKeyDownEvent(event) {
+  if (event.key === 'Enter' && !event.shiftKey) {
+    event.preventDefault();
+    messageInput.value += '\n';
+    adjustMessageInputHeight();
   }
 }
 
-const chatApp = new ChatApp();
-chatApp.init();
+// Toggle model menu display
+function toggleModelMenu() {
+  modelMenu.style.display = (modelMenu.style.display === 'none') ? 'block' : 'none';
+}
+
+// Update selected model and menu
+function selectModel(model) {
+  selectedModel = model;
+  localStorage.setItem('selectedModel', selectedModel);
+  updateModelMenu();
+  updateModelHeading();
+}
+
+// Update model menu selected option
+function updateModelMenu() {
+  const modelOptions = modelMenu.querySelectorAll('ul li');
+  modelOptions.forEach(option => {
+    option.classList.remove('selected');
+    if (option.dataset.model === selectedModel) {
+      option.classList.add('selected');
+    }
+  });
+}
+
+// Update model heading
+function updateModelHeading() {
+  const modelHeading = document.querySelector('h1');
+  modelHeading.textContent = `Chat with ${selectedModel}`;
+}
+
+// Get the number of tokens in a text
+function getTokenCount(text) {
+  const words = text.trim().split(/\s+/);
+  return words.length;
+}
+
+// Display message in chat history
+function createAndAppendMessage(content, owner) {
+  const message = document.createElement('div');
+  message.classList.add('message', owner);
+
+  let displayedText = content;
+
+  const parsedContent = parseResponse(displayedText);
+  message.innerHTML = parsedContent;
+
+  chatHistory.appendChild(message);
+  chatHistory.scrollTop = chatHistory.scrollHeight;
+
+  // Render MathJax equations
+  MathJax.Hub.Queue(['Typeset', MathJax.Hub, message]);
+}
+
+// Parse response to format text content
+function parseResponse(response) {
+  let parsedResponse = response;
+
+  parsedResponse = parsedResponse.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+  parsedResponse = parsedResponse.replace(/\$\$(.*?)\$\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
+  parsedResponse = parsedResponse.replace(/\$(.*?)\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
+  parsedResponse = parseTables(parsedResponse);
+
+  return parsedResponse;
+}
+
+// Parse response to format tables
+function parseTables(response) {
+  const tableRegex = /\n((?:\s*\|.*\|\n)+)\n/g;
+  return response.replace(tableRegex, createTable);
+}
+
+// Create HTML table from table string
+function createTable(match, table) {
+  const rows = table.trim().split('\n');
+  const tableElement = document.createElement('table');
+
+  const tableHeader = document.createElement('tr');
+  const tableHeaderCells = rows[0].split('|').slice(1, -1);
+  tableHeaderCells.forEach((cell) => {
+    const th = document.createElement('th');
+    th.classList.add('table-header');
+    th.textContent = cell.trim();
+    tableHeader.appendChild(th);
+  });
+  tableElement.appendChild(tableHeader);
+
+  for (let i = 2; i < rows.length; i++) {
+    const row = document.createElement('tr');
+    const tableCells = rows[i].split('|').slice(1, -1);
+    tableCells.forEach((cell) => {
+      const td = document.createElement('td');
+      td.classList.add('table-data');
+      td.innerHTML = parseResponse(cell.trim());
+      row.appendChild(td);
+    });
+    tableElement.appendChild(row);
+  }
+
+  return tableElement.outerHTML;
+}
+
+// Send user message and get bot response
+async function sendMessage() {
+  apiKey = apiKeyInput.value.trim();
+  apiEndpoint = apiEndpointInput.value.trim();
+  const message = messageInput.value.trim();
+
+  if (!apiKey || !message) {
+    alert('Please enter your API key and a message.');
+    return;
+  }
+
+  localStorage.setItem('apiKey', apiKey);
+  localStorage.setItem('apiEndpoint', apiEndpoint);
+
+  createAndAppendMessage(message, 'user');
+  messageInput.value = '';
+  messageInput.style.height = 'auto';
+
+  const jsonResponse = await getBotResponse(apiKey, apiEndpoint, message);
+
+  const botResponse = jsonResponse.choices[0].message.content;
+  createAndAppendMessage(botResponse, 'bot');
+}
+
+// Get bot response from API
+async function getBotResponse(apiKey, apiEndpoint, message) {
+  const ENDPOINT = apiEndpoint || "https://chimeragpt.adventblocks.cc/v1/chat/completions";
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${apiKey}`
+  };
+
+  let maxTokens;
+  switch (selectedModel) {
+    case 'gpt-3.5-turbo':
+      maxTokens = 4096;
+      break;
+    case 'gpt-4-poe':
+      maxTokens = 2100;
+      break;
+    case 'gpt-3.5-turbo-16k':
+      maxTokens = 16384;
+      break;
+    case 'gpt-3.5-turbo-0613':
+      maxTokens = 4096;
+      break;
+    case 'gpt-4-0613':
+    case 'gpt-4':
+      maxTokens = 8192;
+      break;
+    case 'claude+':
+    case 'claude-instant':
+    case 'claude-instant-100k':
+      maxTokens = 10240;
+      break;
+    default:
+      maxTokens = 4096;
+  }
+
+  let tokenCount = getTokenCount(messages[0].content);
+  for (let i = 1; i < messages.length; i++) {
+    const messageTokenCount = getTokenCount(messages[i].content);
+    if (tokenCount + messageTokenCount > maxTokens) {
+      messages.splice(1, i - 1);
+      break;
+    }
+    tokenCount += messageTokenCount;
+  }
+
+  messages.push({
+    "role": "user",
+    "content": message
+  });
+
+  aiThinkingMsg.style.display = 'block';
+
+  const data = {
+    "model": selectedModel,
+    "messages": messages
+  };
+
+  const response = await fetch(ENDPOINT, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data)
+  });
+
+  aiThinkingMsg.style.display = 'none';
+
+  return response.json();
+}
+
+// Copy latest bot response to clipboard
+function copyLatestResponse() {
+  const latestResponse = chatHistory.lastElementChild.innerHTML;
+  if (latestResponse) {
+    copyToClipboard(latestResponse);
+    alert('Text copied to clipboard');
+  } else {
+    alert('No text to copy');
+  }
+}
+
+// Copy text to clipboard
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+
+// Update system role message
+function updateSystemRoleMessage() {
+  localStorage.setItem('systemRole', systemRoleInput.value);
+  messages[0].content = systemRoleInput.value;
+  }
