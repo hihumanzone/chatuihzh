e7a2c0571b52773515b2ceeb6829013e901fc09c
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-08-21 20:03:42 +0530

    Create ras.js

---
+++ ras.js
@@ -0,0 +1,94 @@
+let messages = [
+  {
+    role: 'system',
+    content: localStorage.getItem('systemRole') || '',
+  },
+];
+
+let apiKey = localStorage.getItem('apiKey') || '';
+let apiEndpoint = localStorage.getItem('apiEndpoint') || '';
+let selectedModel = localStorage.getItem('selectedModel') || 'gpt-3.5-turbo';
+
+const ENDPOINT = apiEndpoint || 'https://free.churchless.tech/v1/chat/completions';
+
+async function getBotResponse(apiKey, apiEndpoint, message) {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${apiKey}`,
+  };
+
+  messages.push({
+    role: 'user',
+    content: message,
+  });
+
+  aiThinkingMsg.style.display = 'block';
+
+  const data = {
+    model: selectedModel,
+    messages: messages,
+  };
+
+  const response = await fetch(ENDPOINT, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
+  });
+
+  aiThinkingMsg.style.display = 'none';
+
+  return response.json();
+}
+
+function toggleModelMenu() {
+  modelMenu.style.display = modelMenu.style.display === 'none' ? 'block' : 'none';
+}
+
+function selectModel(model) {
+  const modelOptions = document.querySelectorAll('ul li');
+  modelOptions.forEach((option) => option.classList.remove('selected'));
+
+  const selectedModelOption = document.querySelector(`ul li[data-model="${model}"]`);
+  if (selectedModelOption) {
+    selectedModelOption.classList.add('selected');
+  }
+
+  selectedModel = model;
+  localStorage.setItem('selectedModel', selectedModel);
+
+  toggleModelMenu();
+  updateModelHeading();
+}
+
+function updateModelHeading() {
+  const modelHeading = document.querySelector('h1');
+  modelHeading.textContent = `Chat with ${selectedModel}`;
+}
+
+async function sendMessage() {
+  apiKey = apiKeyInput.value.trim();
+  apiEndpoint = apiEndpointInput.value.trim();
+  const message = messageInput.value.trim();
+
+  if (!message) {
+    alert('Please enter a message.');
+    return;
+  }
+
+  localStorage.setItem('apiKey', apiKey);
+  localStorage.setItem('apiEndpoint', apiEndpoint);
+
+  createAndAppendMessage(message, 'user');
+  messageInput.value = '';
+  messageInput.style.height = 'auto';
+
+  const jsonResponse = await getBotResponse(apiKey, apiEndpoint, message);
+
+  const botResponse = jsonResponse.choices[0].message.content;
+  messages.push({
+    role: 'assistant',
+    content: botResponse,
+  });
+
+  createAndAppendMessage(botResponse, 'bot');
+}
