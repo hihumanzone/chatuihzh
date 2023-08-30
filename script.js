9783d7fc85a1683de9b88003ed9e92c904044c78
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-08-30 12:43:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,52 +13,54 @@ const headingRegex = [
   /^####\s(.+)/gm
 ];
 
+const localStore = localStorage;
+
+let apiKey = localStore.getItem('apiKey') || '';
+let apiEndpoint = localStore.getItem('apiEndpoint') || 'https://free.churchless.tech/v1/chat/completions';
+let selectedModel = localStore.getItem('selectedModel') || 'gpt-3.5-turbo';
+
 let messages = [
   {
     role: 'system',
-    content: localStorage.getItem('systemRole') || '',
+    content: localStore.getItem('systemRole') || '',
   },
 ];
-
-let apiKey = localStorage.getItem('apiKey') || '';
-let apiEndpoint = localStorage.getItem('apiEndpoint') || '';
-let selectedModel = localStorage.getItem('selectedModel') || 'gpt-3.5-turbo';
 
 apiKeyInput.value = apiKey;
 apiEndpointInput.value = apiEndpoint;
 selectModel(selectedModel);
 updateModelHeading();
 
-messageInput.addEventListener('input', () => {
+messageInput.addEventListener('input', handleInputChange);
+messageInput.addEventListener('keydown', handleKeydownEvent);
+document.getElementById('send-button').addEventListener('click', sendMessage);
+
+function handleInputChange() {
   messageInput.style.height = 'auto';
   messageInput.style.height = `${messageInput.scrollHeight}px`;
-});
+};
 
-messageInput.addEventListener('keydown', (event) => {
+function handleKeydownEvent(event) {
   if (event.key === 'Enter' && !event.shiftKey) {
     event.preventDefault();
     messageInput.value += '\n';
     messageInput.style.height = `${messageInput.scrollHeight}px`;
   }
-});
-
-document.getElementById('send-button').addEventListener('click', sendMessage);
+}
 
 function toggleModelMenu() {
   modelMenu.style.display = modelMenu.style.display === 'none' ? 'block' : 'none';
 }
 
 function selectModel(model) {
-  const modelOptions = document.querySelectorAll('ul li');
-  modelOptions.forEach((option) => option.classList.remove('selected'));
-
-  const selectedModelOption = document.querySelector(`ul li[data-model="${model}"]`);
-  if (selectedModelOption) {
-    selectedModelOption.classList.add('selected');
-  }
+  modelOptions.forEach((option) => {
+    const { classList } = option;
+    classList.remove('selected');
+    if(option.dataset.model == model) classList.add('selected');
+  });
 
   selectedModel = model;
-  localStorage.setItem('selectedModel', selectedModel);
+  localStore.setItem('selectedModel', selectedModel);
 
   toggleModelMenu();
   updateModelHeading();
@@ -69,9 +71,7 @@ function updateModelHeading() {
   modelHeading.textContent = `Chat with ${selectedModel}`;
 }
 
-const ENDPOINT = apiEndpoint || 'https://free.churchless.tech/v1/chat/completions';
-
-async function getBotResponse(apiKey, apiEndpoint, message) {
+async function getBotResponse(message) {
   const headers = {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${apiKey}`,
@@ -89,7 +89,7 @@ async function getBotResponse(apiKey, apiEndpoint, message) {
     messages: messages,
   };
 
-  const response = await fetch(ENDPOINT, {
+  const response = await fetch(apiEndpoint, {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(data),
@@ -100,129 +100,7 @@ async function getBotResponse(apiKey, apiEndpoint, message) {
   return response.json();
 }
 
-function extractCodeBlocks(response) {
-  const codeBlocks = response.match(codeBlockRegex);
-  if (codeBlocks) {
-    codeBlocks.forEach((codeBlock) => {
-      const codeWithoutMarkdown = codeBlock.replace(/```/g, '');
-      response = response.replace(codeBlock, '```' + codeWithoutMarkdown + '```');
-    });
-  }
-  return response;
-}
-
-function createCodeBlockUI(codeBlock) {
-  const preElement = document.createElement('pre');
-  preElement.textContent = codeBlock.replace(/```/g, '');
-
-  const codeBlockElement = document.createElement('div');
-  codeBlockElement.classList.add('code-block');
-  codeBlockElement.appendChild(preElement);
-
-  const copyCodeButton = document.createElement('button');
-  copyCodeButton.classList.add('copy-code-button');
-  copyCodeButton.textContent = 'Copy The Code';
-  codeBlockElement.appendChild(copyCodeButton);
-
-  return codeBlockElement.outerHTML;
-}
-
-async function createAndAppendMessage(content, owner) {
-  const message = document.createElement('div');
-  message.classList.add('message', owner);
-
-  let displayedText = content;
-  
-  if (owner === 'bot') {
-    if (displayedText.startsWith('>')) {
-      message.style.backgroundColor = '#222';
-      message.style.borderColor = '#555';
-    } else {
-      displayedText = extractCodeBlocks(displayedText);
-      if (displayedText.startsWith('`') && displayedText.endsWith('`')) {
-        message.style.backgroundColor = '#333';
-      }
-    }
-  }
-
-  const parsedContent = parseResponse(displayedText);
-  message.innerHTML = parsedContent;
-
-  chatHistory.appendChild(message);
-  chatHistory.scrollTop = chatHistory.scrollHeight;
-
-  MathJax.Hub.Queue(['Typeset', MathJax.Hub, message]);
-}
-
-function parseResponse(response) {
-  let parsedResponse = response;
-
-  const codeBlocks = parsedResponse.match(codeBlockRegex);
-  if (codeBlocks) {
-    codeBlocks.forEach((codeBlock, index) => {
-      parsedResponse = parsedResponse.replace(codeBlock, `CODEBLOCK${index}`);
-    });
-  }
-
-  parsedResponse = parsedResponse.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-  parsedResponse = parsedResponse.replace(/\$\$(.*?)\$\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
-  parsedResponse = parsedResponse.replace(/\$(.*?)\$/g, '<span class="mathjax-latex">\\($1\\)</span>');
-  parsedResponse = parseTables(parsedResponse);
-
-  headingRegex.forEach((regex, index) => {
-    const fontSize = 30 - (index * 4);
-    const fontWeight = index === 0 ? 'bold' : 'normal';
-    parsedResponse = parsedResponse.replace(regex, `<span style="font-size: ${fontSize}px; font-weight: ${fontWeight};">$1</span>`);
-  });
-
-  parsedResponse = parsedResponse.replace(/\*(.*?)\*/g, '<i>$1</i>');
-
-  if (codeBlocks) {
-    codeBlocks.forEach((codeBlock, index) => {
-      parsedResponse = parsedResponse.replace(`CODEBLOCK${index}`, createCodeBlockUI(codeBlock));
-    });
-  }
-
-  return parsedResponse;
-}
-
-function parseTables(response) {
-  const tableRegex = /\n((?:\s*:?[\|:].*?:?\|\n)+)\n/g;
-  return response.replace(tableRegex, createTable);
-}
-
-function createTable(match, table) {
-  const rows = table.trim().split('\n');
-  const tableElement = document.createElement('table');
-
-  const tableHeader = document.createElement('tr');
-  const tableHeaderCells = rows[0].split('|').slice(1, -1);
-  tableHeaderCells.forEach((cell) => {
-    const th = document.createElement('th');
-    th.classList.add('table-header');
-    th.textContent = cell.trim();
-    tableHeader.appendChild(th);
-  });
-  tableElement.appendChild(tableHeader);
-
-  for (let i = 2; i < rows.length; i++) {
-    const row = document.createElement('tr');
-    const tableCells = rows[i].split('|').slice(1, -1);
-    tableCells.forEach((cell) => {
-      const td = document.createElement('td');
-      td.classList.add('table-data');
-      td.innerHTML = parseResponse(cell.trim());
-      row.appendChild(td);
-    });
-    tableElement.appendChild(row);
-  }
-
-  return `\n${tableElement.outerHTML}\n`;
-}
-
 async function sendMessage() {
-  apiKey = apiKeyInput.value.trim();
-  apiEndpoint = apiEndpointInput.value.trim();
   const message = messageInput.value.trim();
 
   if (!message) {
@@ -230,16 +108,17 @@ async function sendMessage() {
     return;
   }
 
-  localStorage.setItem('apiKey', apiKey);
-  localStorage.setItem('apiEndpoint', apiEndpoint);
+  localStore.setItem('apiKey', apiKey);
+  localStore.setItem('apiEndpoint', apiEndpoint);
 
   createAndAppendMessage(message, 'user');
   messageInput.value = '';
   messageInput.style.height = 'auto';
 
-  const jsonResponse = await getBotResponse(apiKey, apiEndpoint, message);
+  const jsonResponse = await getBotResponse(message);
 
   const botResponse = jsonResponse.choices[0].message.content;
+
   messages.push({
     role: 'assistant',
     content: botResponse,
@@ -248,49 +127,12 @@ async function sendMessage() {
   createAndAppendMessage(botResponse, 'bot');
 }
 
-function copyToClipboard(text) {
-  const textarea = document.createElement('textarea');
-  textarea.value = text;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textarea);
-}
-
-function clearChatHistory() {
-  chatHistory.innerHTML = '';
-  messages = [
-    {
-      role: 'system',
-      content: localStorage.getItem('systemRole') || '',
-    },
-  ];
-}
-
-document.getElementById('copy-button').addEventListener('click', () => {
-  const latestResponse = chatHistory.lastElementChild.innerHTML;
-  if (latestResponse) {
-    copyToClipboard(latestResponse);
-    alert('Text copied to clipboard');
-  } else {
-    alert('No text to copy');
-  }
-});
-
-systemRoleInput.value = localStorage.getItem('systemRole') || '';
-systemRoleInput.addEventListener('input', () => {
-  localStorage.setItem('systemRole', systemRoleInput.value);
-  messages[0].content = systemRoleInput.value;
-});
-
-window.addEventListener('load', updateModelHeading);
-
 function saveInputsAndRefresh() {
   apiKey = apiKeyInput.value.trim();
   apiEndpoint = apiEndpointInput.value.trim();
 
-  localStorage.setItem('apiKey', apiKey);
-  localStorage.setItem('apiEndpoint', apiEndpoint);
+  localStore.setItem('apiKey', apiKey);
+  localStore.setItem('apiEndpoint', apiEndpoint);
 
   location.reload();
 }
