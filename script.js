0c85e16c6b9ff3934f3838579315429e4d6fe119
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-10-10 13:42:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ async function createAndAppendMessage(content, owner) {
   const md = window.markdownit();
   displayedText = md.render(displayedText);
   message.innerHTML = displayedText;
-  chatHistory.appendChild(message);
+  chatHistory.insertBefore(message, aiThinkingMsg);
   chatHistory.scrollTop = chatHistory.scrollHeight;
   MathJax.Hub.Queue(['Typeset', MathJax.Hub, message]);
   addCopyButtonToCodeBlock();
